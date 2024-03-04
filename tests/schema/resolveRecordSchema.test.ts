a9af6fcbfc4d4b335fc3d9bf7d5f904faea03c20
curl girl <curl_girl@curls.local>
2024-03-04 08:55:20 -0300

    chore(tests): progress

--- tests/schema/resolveRecordSchema.test.ts
+++ tests/schema/resolveRecordSchema.test.ts
@@ -1,41 +1,88 @@
 import {
   resolveInitialRecordSchema,
   resolveNextRecordSchema,
+  SolidifiedSchema,
 } from '../../source/library/module.ts';
 import { Assert } from '../imports/Assert.ts';
 
-Deno.test({ name: 'resolveInitialRecordSchema' }, () => {
-  const basicInitialRecordSchema = resolveInitialRecordSchema({
-    solidifiedSchema: {
-      schemaSymbol: 'BasicSchema__EXAMPLE',
-      schemaMap: {
-        BasicDataModel: {
-          modelSymbolKey: 'BasicDataModel',
-          modelProperties: {
-            aaaProperty_EXAMPLE: {
-              propertyKey: 'aaaProperty_EXAMPLE',
-              propertyElement: {
-                elementKind: 'booleanPrimitive',
-              },
-            },
-            bbbProperty_EXAMPLE: {
-              propertyKey: 'bbbProperty_EXAMPLE',
-              propertyElement: {
-                elementKind: 'booleanPrimitive',
-              },
-            },
-            cccProperty_EXAMPLE: {
-              propertyKey: 'cccProperty_EXAMPLE',
-              propertyElement: {
-                elementKind: 'booleanPrimitive',
-              },
-            },
-          },
-        },
-      },
-    },
+Deno.test({ name: 'resolveInitialRecordSchema' }, async (testContext) => {
+  const inputSolidifiedSchema = getInitialSolidifiedSchema();
+  const resolveRecordSchema = resolveInitialRecordSchema({
+    solidifiedSchema: inputSolidifiedSchema,
   });
-  Assert.assertEquals(basicInitialRecordSchema, {
+  const resolvedRecordModel = resolveRecordSchema.schemaMap['BasicDataModel']!;
+  const inputSolidifiedModel = inputSolidifiedSchema
+    .schemaMap['BasicDataModel']!;
+  await testContext.step('input model data applied', () => {
+    Assert.assertEquals(
+      resolvedRecordModel.modelSymbolKey,
+      inputSolidifiedModel.modelSymbolKey,
+    );
+    Assert.assertEquals(
+      resolvedRecordModel.modelProperties,
+      inputSolidifiedModel.modelProperties,
+    );
+  });
+  await testContext.step('model encoding', async (subTestContext) => {
+    await subTestContext.step('all properties sorted and entered', () => {
+      Assert.assertEquals(
+        resolvedRecordModel.modelEncoding,
+        [
+          {
+            entryPropertyKey: 'aaaProperty_EXAMPLE',
+          },
+          {
+            entryPropertyKey: 'bbbProperty_EXAMPLE',
+          },
+          {
+            entryPropertyKey: 'cccProperty_EXAMPLE',
+          },
+        ],
+      );
+    });
+  });
+});
+
+Deno.test({ name: 'resolveNextRecordSchema' }, async (testContext) => {
+  const updatedRecordSchema = resolveNextRecordSchema({
+    staleRecordSchema: resolveInitialRecordSchema({
+      solidifiedSchema: getInitialSolidifiedSchema(),
+    }),
+    solidifiedSchema: getUpdatedSolidifiedSchema(),
+  });
+  const updatedRecordModel = updatedRecordSchema.schemaMap['BasicDataModel']!;
+  await testContext.step('model encoding', async (subTestContext) => {
+    await subTestContext.step(
+      'remaining pre-existing properties prepended',
+      () => {
+        Assert.assertEquals(updatedRecordModel.modelEncoding.slice(0, 2), [
+          {
+            entryPropertyKey: 'bbbProperty_EXAMPLE',
+          },
+          {
+            entryPropertyKey: 'cccProperty_EXAMPLE',
+          },
+        ]);
+      },
+    );
+    await subTestContext.step(
+      'new properties sorted and appended',
+      () => {
+        Assert.assertEquals(updatedRecordModel.modelEncoding.slice(2, 4), [
+          {
+            entryPropertyKey: 'aaaUpdatedProperty_EXAMPLE',
+          },
+          {
+            entryPropertyKey: 'dddProperty_EXAMPLE',
+          },
+        ]);
+      },
+    );
+  });
+});
+
+function getInitialSolidifiedSchema(): SolidifiedSchema {
+  return {
     schemaSymbol: 'BasicSchema__EXAMPLE',
     schemaMap: {
       BasicDataModel: {
@@ -60,93 +107,13 @@ Deno.test({ name: 'resolveInitialRecordSchema' }, () => {
             },
           },
         },
-        modelEncoding: [
-          {
-            entryPropertyKey: 'aaaProperty_EXAMPLE',
-          },
-          {
-            entryPropertyKey: 'bbbProperty_EXAMPLE',
-          },
-          {
-            entryPropertyKey: 'cccProperty_EXAMPLE',
-          },
-        ],
       },
     },
-  });
-});
+  };
+}
 
-Deno.test({ name: 'resolveNextRecordSchema' }, () => {
-  const basicNextRecordSchema = resolveNextRecordSchema({
-    staleRecordSchema: {
-      schemaSymbol: 'BasicSchema__EXAMPLE',
-      schemaMap: {
-        BasicDataModel: {
-          modelSymbolKey: 'BasicDataModel',
-          modelProperties: {
-            aaaProperty_EXAMPLE: {
-              propertyKey: 'aaaProperty_EXAMPLE',
-              propertyElement: {
-                elementKind: 'booleanPrimitive',
-              },
-            },
-            bbbProperty_EXAMPLE: {
-              propertyKey: 'bbbProperty_EXAMPLE',
-              propertyElement: {
-                elementKind: 'booleanPrimitive',
-              },
-            },
-            cccProperty_EXAMPLE: {
-              propertyKey: 'cccProperty_EXAMPLE',
-              propertyElement: {
-                elementKind: 'booleanPrimitive',
-              },
-            },
-          },
-          modelEncoding: [
-            {
-              entryPropertyKey: 'aaaProperty_EXAMPLE',
-            },
-            {
-              entryPropertyKey: 'bbbProperty_EXAMPLE',
-            },
-            {
-              entryPropertyKey: 'cccProperty_EXAMPLE',
-            },
-          ],
-        },
-      },
-    },
-    solidifiedSchema: {
-      schemaSymbol: 'BasicSchema__EXAMPLE',
-      schemaMap: {
-        BasicDataModel: {
-          modelSymbolKey: 'BasicDataModel',
-          modelProperties: {
-            bbbProperty_EXAMPLE: {
-              propertyKey: 'bbbProperty_EXAMPLE',
-              propertyElement: {
-                elementKind: 'booleanPrimitive',
-              },
-            },
-            cccProperty_EXAMPLE: {
-              propertyKey: 'cccProperty_EXAMPLE',
-              propertyElement: {
-                elementKind: 'booleanPrimitive',
-              },
-            },
-            aaaUpdatedProperty_EXAMPLE: {
-              propertyKey: 'aaaUpdatedProperty_EXAMPLE',
-              propertyElement: {
-                elementKind: 'booleanPrimitive',
-              },
-            },
-          },
-        },
-      },
-    },
-  });
-  Assert.assertEquals(basicNextRecordSchema, {
+function getUpdatedSolidifiedSchema(): SolidifiedSchema {
+  return {
     schemaSymbol: 'BasicSchema__EXAMPLE',
     schemaMap: {
       BasicDataModel: {
@@ -170,19 +137,14 @@ Deno.test({ name: 'resolveNextRecordSchema' }, () => {
               elementKind: 'booleanPrimitive',
             },
           },
+          dddProperty_EXAMPLE: {
+            propertyKey: 'dddProperty_EXAMPLE',
+            propertyElement: {
+              elementKind: 'booleanPrimitive',
+            },
+          },
         },
-        modelEncoding: [
-          {
-            entryPropertyKey: 'bbbProperty_EXAMPLE',
-          },
-          {
-            entryPropertyKey: 'cccProperty_EXAMPLE',
-          },
-          {
-            entryPropertyKey: 'aaaUpdatedProperty_EXAMPLE',
-          },
-        ],
       },
     },
-  });
-});
+  };
+}
--- tests/schema/resolveSolidifiedSchema.test.ts
+++ tests/schema/resolveSolidifiedSchema.test.ts
@@ -2,195 +2,210 @@ import { resolveSolidifiedSchema } from '../../source/library/module.ts';
 import { Assert } from '../imports/Assert.ts';
 
 Deno.test({ name: 'resolveSolidifiedSchema' }, async (testContext) => {
-    const compositeDataModelSolidifiedSchema = resolveSolidifiedSchema({
-      intermediateSchema: {
-        schemaSymbol: 'CompositeDataModelSchema__EXAMPLE',
-        schemaMap: {
-          data: {
-            CompositeDataModel__EXAMPLE: {
-              modelKind: 'data',
-              modelSymbolKey: 'CompositeDataModel__EXAMPLE',
-              modelTemplates: [
-                {
-                  templateKind: 'concreteTemplate',
-                  templateModelSymbolKey: 'ConcreteTemplateModel__EXAMPLE',
-                },
-                {
-                  templateKind: 'genericTemplate',
-                  templateModelSymbolKey:
-                    'CompositeGenericTemplateModel__EXAMPLE',
-                  genericArguments: {
-                    IndirectParameter__EXAMPLE: {
-                      argumentIndex: 0,
-                      argumentParameterSymbolKey: 'IndirectParameter__EXAMPLE',
-                      argumentElement: {
-                        elementKind: 'booleanPrimitive',
-                      },
+  const resolvedSolidifiedSchema = resolveSolidifiedSchema({
+    intermediateSchema: {
+      schemaSymbol: 'CompositeDataModelSchema__EXAMPLE',
+      schemaMap: {
+        data: {
+          CompositeDataModel__EXAMPLE: {
+            modelKind: 'data',
+            modelSymbolKey: 'CompositeDataModel__EXAMPLE',
+            modelTemplates: [
+              {
+                templateKind: 'concreteTemplate',
+                templateModelSymbolKey: 'ConcreteTemplateModel__EXAMPLE',
+              },
+              {
+                templateKind: 'genericTemplate',
+                templateModelSymbolKey:
+                  'CompositeGenericTemplateModel__EXAMPLE',
+                genericArguments: {
+                  IndirectParameter__EXAMPLE: {
+                    argumentIndex: 0,
+                    argumentParameterSymbolKey: 'IndirectParameter__EXAMPLE',
+                    argumentElement: {
+                      elementKind: 'booleanPrimitive',
                     },
                   },
                 },
-              ],
-              modelProperties: {
-                dataModelProperty__EXAMPLE: {
-                  propertyKey: 'dataModelProperty__EXAMPLE',
-                  propertyElement: {
-                    elementKind: 'booleanPrimitive',
-                  },
+              },
+            ],
+            modelProperties: {
+              dataModelProperty__EXAMPLE: {
+                propertyKey: 'dataModelProperty__EXAMPLE',
+                propertyElement: {
+                  elementKind: 'booleanPrimitive',
                 },
               },
             },
           },
-          concreteTemplate: {
-            ConcreteTemplateModel__EXAMPLE: {
-              modelKind: 'concreteTemplate',
-              modelSymbolKey: 'ConcreteTemplateModel__EXAMPLE',
-              modelTemplates: [],
-              modelProperties: {
-                concreteTemplateModelProperty__EXAMPLE: {
-                  propertyKey: 'concreteTemplateModelProperty__EXAMPLE',
-                  propertyElement: {
-                    elementKind: 'booleanPrimitive',
-                  },
+        },
+        concreteTemplate: {
+          ConcreteTemplateModel__EXAMPLE: {
+            modelKind: 'concreteTemplate',
+            modelSymbolKey: 'ConcreteTemplateModel__EXAMPLE',
+            modelTemplates: [],
+            modelProperties: {
+              concreteTemplateModelProperty__EXAMPLE: {
+                propertyKey: 'concreteTemplateModelProperty__EXAMPLE',
+                propertyElement: {
+                  elementKind: 'booleanPrimitive',
                 },
               },
             },
           },
-          genericTemplate: {
-            TerminalGenericTemplateModel__EXAMPLE: {
-              modelKind: 'genericTemplate',
-              modelSymbolKey: 'TerminalGenericTemplateModel__EXAMPLE',
-              genericParameters: [
-                {
+        },
+        genericTemplate: {
+          TerminalGenericTemplateModel__EXAMPLE: {
+            modelKind: 'genericTemplate',
+            modelSymbolKey: 'TerminalGenericTemplateModel__EXAMPLE',
+            genericParameters: [
+              {
+                parameterSymbol: 'BasicParameter__EXAMPLE',
+              },
+              {
+                parameterSymbol: 'ConstrainedParameter__EXAMPLE',
+              },
+            ],
+            modelTemplates: [],
+            modelProperties: {
+              terminalGenericTemplateModelProperty__EXAMPLE: {
+                propertyKey: 'terminalGenericTemplateModelProperty__EXAMPLE',
+                propertyElement: {
+                  elementKind: 'booleanPrimitive',
+                },
+              },
+              basicParameterProperty__EXAMPLE: {
+                propertyKey: 'basicParameterProperty__EXAMPLE',
+                propertyElement: {
+                  elementKind: 'basicParameter',
                   parameterSymbol: 'BasicParameter__EXAMPLE',
                 },
-                {
+              },
+              constrainedParameterProperty__EXAMPLE: {
+                propertyKey: 'constrainedParameterProperty__EXAMPLE',
+                propertyElement: {
+                  elementKind: 'constrainedParameter',
                   parameterSymbol: 'ConstrainedParameter__EXAMPLE',
-                },
-              ],
-              modelTemplates: [],
-              modelProperties: {
-                terminalGenericTemplateModelProperty__EXAMPLE: {
-                  propertyKey: 'terminalGenericTemplateModelProperty__EXAMPLE',
-                  propertyElement: {
-                    elementKind: 'booleanPrimitive',
-                  },
-                },
-                basicParameterProperty__EXAMPLE: {
-                  propertyKey: 'basicParameterProperty__EXAMPLE',
-                  propertyElement: {
-                    elementKind: 'basicParameter',
-                    parameterSymbol: 'BasicParameter__EXAMPLE',
-                  },
-                },
-                constrainedParameterProperty__EXAMPLE: {
-                  propertyKey: 'constrainedParameterProperty__EXAMPLE',
-                  propertyElement: {
-                    elementKind: 'constrainedParameter',
-                    parameterSymbol: 'ConstrainedParameter__EXAMPLE',
-                  },
                 },
               },
             },
-            CompositeGenericTemplateModel__EXAMPLE: {
-              modelKind: 'genericTemplate',
-              modelSymbolKey: 'CompositeGenericTemplateModel__EXAMPLE',
-              genericParameters: [
-                {
-                  parameterSymbol: 'IndirectParameter__EXAMPLE',
-                },
-              ],
-              modelTemplates: [
-                {
-                  templateKind: 'genericTemplate',
-                  templateModelSymbolKey:
-                    'TerminalGenericTemplateModel__EXAMPLE',
-                  genericArguments: {
-                    BasicParameter__EXAMPLE: {
-                      argumentIndex: 0,
-                      argumentParameterSymbolKey: 'BasicParameter__EXAMPLE',
-                      argumentElement: {
-                        elementKind: 'basicParameter',
-                        parameterSymbol: 'IndirectParameter__EXAMPLE',
-                      },
+          },
+          CompositeGenericTemplateModel__EXAMPLE: {
+            modelKind: 'genericTemplate',
+            modelSymbolKey: 'CompositeGenericTemplateModel__EXAMPLE',
+            genericParameters: [
+              {
+                parameterSymbol: 'IndirectParameter__EXAMPLE',
+              },
+            ],
+            modelTemplates: [
+              {
+                templateKind: 'genericTemplate',
+                templateModelSymbolKey: 'TerminalGenericTemplateModel__EXAMPLE',
+                genericArguments: {
+                  BasicParameter__EXAMPLE: {
+                    argumentIndex: 0,
+                    argumentParameterSymbolKey: 'BasicParameter__EXAMPLE',
+                    argumentElement: {
+                      elementKind: 'basicParameter',
+                      parameterSymbol: 'IndirectParameter__EXAMPLE',
                     },
-                    ConstrainedParameter__EXAMPLE: {
-                      argumentIndex: 1,
-                      argumentParameterSymbolKey:
-                        'ConstrainedParameter__EXAMPLE',
-                      argumentElement: {
-                        elementKind: 'booleanPrimitive',
-                      },
+                  },
+                  ConstrainedParameter__EXAMPLE: {
+                    argumentIndex: 1,
+                    argumentParameterSymbolKey: 'ConstrainedParameter__EXAMPLE',
+                    argumentElement: {
+                      elementKind: 'booleanPrimitive',
                     },
                   },
                 },
-              ],
-              modelProperties: {
-                compositeGenericTemplateModelProperty__EXAMPLE: {
-                  propertyKey: 'compositeGenericTemplateModelProperty__EXAMPLE',
-                  propertyElement: {
-                    elementKind: 'booleanPrimitive',
-                  },
+              },
+            ],
+            modelProperties: {
+              compositeGenericTemplateModelProperty__EXAMPLE: {
+                propertyKey: 'compositeGenericTemplateModelProperty__EXAMPLE',
+                propertyElement: {
+                  elementKind: 'booleanPrimitive',
                 },
               },
             },
           },
         },
       },
-    });
-    await testContext.step('generic parameter argument resolved', () => {
+    },
+  });
+  const resolvedSoldifiedModel = resolvedSolidifiedSchema
+    .schemaMap['CompositeDataModel__EXAMPLE']!;
+  await testContext.step('property resolution', async (subTestContext) => {
+    await subTestContext.step('top-level property', () => {
       Assert.assertEquals(
-        compositeDataModelSolidifiedSchema
-          .schemaMap['CompositeDataModel__EXAMPLE']
-          ?.modelProperties['basicParameterProperty__EXAMPLE']?.propertyElement,
+        resolvedSoldifiedModel
+          .modelProperties['dataModelProperty__EXAMPLE']?.propertyElement,
         {
           elementKind: 'booleanPrimitive',
         },
       );
     });
-    Assert.assertEquals(compositeDataModelSolidifiedSchema, {
-      schemaSymbol: 'CompositeDataModelSchema__EXAMPLE',
-      schemaMap: {
-        CompositeDataModel__EXAMPLE: {
-          modelSymbolKey: 'CompositeDataModel__EXAMPLE',
-          modelProperties: {
-            dataModelProperty__EXAMPLE: {
-              propertyKey: 'dataModelProperty__EXAMPLE',
-              propertyElement: {
-                elementKind: 'booleanPrimitive',
-              },
-            },
-            concreteTemplateModelProperty__EXAMPLE: {
-              propertyKey: 'concreteTemplateModelProperty__EXAMPLE',
-              propertyElement: {
-                elementKind: 'booleanPrimitive',
-              },
-            },
-            compositeGenericTemplateModelProperty__EXAMPLE: {
-              propertyKey: 'compositeGenericTemplateModelProperty__EXAMPLE',
-              propertyElement: {
-                elementKind: 'booleanPrimitive',
-              },
-            },
-            terminalGenericTemplateModelProperty__EXAMPLE: {
-              propertyKey: 'terminalGenericTemplateModelProperty__EXAMPLE',
-              propertyElement: {
-                elementKind: 'booleanPrimitive',
-              },
-            },
-            basicParameterProperty__EXAMPLE: {
-              propertyKey: 'basicParameterProperty__EXAMPLE',
-              propertyElement: {
-                elementKind: 'booleanPrimitive',
-              },
-            },
-            constrainedParameterProperty__EXAMPLE: {
-              propertyKey: 'constrainedParameterProperty__EXAMPLE',
-              propertyElement: {
-                elementKind: 'booleanPrimitive',
-              },
-            },
-          },
+    await subTestContext.step('concrete template property', () => {
+      Assert.assertEquals(
+        resolvedSoldifiedModel
+          .modelProperties['concreteTemplateModelProperty__EXAMPLE']
+          ?.propertyElement,
+        {
+          elementKind: 'booleanPrimitive',
         },
-      },
+      );
     });
+    await subTestContext.step('generic template property', () => {
+      Assert.assertEquals(
+        resolvedSoldifiedModel
+          .modelProperties['compositeGenericTemplateModelProperty__EXAMPLE']
+          ?.propertyElement,
+        {
+          elementKind: 'booleanPrimitive',
+        },
+      );
+    });
+    await subTestContext.step('deep template property', () => {
+      Assert.assertEquals(
+        resolvedSoldifiedModel
+          .modelProperties['terminalGenericTemplateModelProperty__EXAMPLE']
+          ?.propertyElement,
+        {
+          elementKind: 'booleanPrimitive',
+        },
+      );
+    });
+  });
+  await testContext.step('parameter resolution', async (subTestContext) => {
+    await subTestContext.step('parameter argument', () => {
+      Assert.assertEquals(
+        resolvedSoldifiedModel
+          .modelProperties['basicParameterProperty__EXAMPLE']?.propertyElement,
+        {
+          elementKind: 'booleanPrimitive',
+        },
+      );
+    });
+    await subTestContext.step('basic parameter property', () => {
+      Assert.assertEquals(
+        resolvedSoldifiedModel
+          .modelProperties['basicParameterProperty__EXAMPLE']?.propertyElement,
+        {
+          elementKind: 'booleanPrimitive',
+        },
+      );
+    });
+    await subTestContext.step('constrained parameter property', () => {
+      Assert.assertEquals(
+        resolvedSoldifiedModel
+          .modelProperties['constrainedParameterProperty__EXAMPLE']
+          ?.propertyElement,
+        {
+          elementKind: 'booleanPrimitive',
+        },
+      );
+    });
+  });
 });
